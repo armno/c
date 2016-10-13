ead65fadc6ec2441895aecc1cdaff3af72b844a7
Armno <monkeyarmno@gmail.com>
2016-10-13 22:37:45 +0700

    WIP: make it updates data only when it has to

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const rp = require('request-promise');
+const moment = require('moment');
 
 const BASEURL = `https://www.strava.com/api/v3`;
 const config = require('../config');
@@ -30,6 +32,28 @@ router.get('/', function(req, res, next) {
 		db.collection('elevation').find().toArray((err, result) => {
 			if (err) {
 				throw err;
+			}
+			console.log(result);
+
+			if (result.length !== 0) {
+				// there is an existing record
+				// if last updated date was longer than x days,
+				const updatedAt = moment(result[result.length - 1].updatedAt);
+				const now = moment();
+				const interval = 100; // 10 seconds
+				const timeDiff = now.diff(updatedAt, 'seconds');
+				console.log(`last update was ${timeDiff} seconds ago.`);
+
+				if (timeDiff > interval) {
+					console.info('data was too old. updating ...');
+					// get fresh data from strava and save into db
+					getElevationFromStrava()
+						.then(elevation => {
+							saveCurrentElevation(db, elevation);
+						});
+				} else {
+					console.info('still fresh data na');
+				}
 			}
 
 		 if (result.length === 0) {
@@ -81,5 +105,31 @@ router.get('/', function(req, res, next) {
 	 });
 	});
 });
+
+function getElevationFromStrava() {
+	const options = {
+		uri: `${BASEURL}/athletes/${config.PROFILE_ID}/stats`,
+		headers: {
+			Authorization: `Bearer ${config.API_KEY}`
+		},
+		json: true
+	};
+
+	return rp(options).then(response => response.all_ride_totals.elevation_gain);
+}
+
+function saveCurrentElevation(db, elevation) {
+	const data = {
+		name: elevation,
+		updatedAt: moment().unix()
+	}
+	db.collection('elevation').save(data, (err, result) => {
+		if (err) {
+			return console.error(err);
+		}
+
+		console.log('saved into db');
+	});
+}
 
 module.exports = router;
