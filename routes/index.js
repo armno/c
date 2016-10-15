58d4a569042624de995251502da9f044804fae3b
Armno <monkeyarmno@gmail.com>
2016-10-15 09:56:47 +0700

    remove unused code. finish the feature.

--- routes/index.js
+++ routes/index.js
@@ -33,80 +33,62 @@ router.get('/', function(req, res, next) {
 			if (err) {
 				throw err;
 			}
-			console.log(result[result.length - 1]);
 
 			if (result.length !== 0) {
 				// there is an existing record
 				// if last updated date was longer than x days,
-				const updatedAt = moment(result[result.length - 1].updatedAt);
-				const interval = 1; // 1 day
-				const timeDiff = moment().diff(updatedAt, 'days');
-				console.log(`last update was ${timeDiff} days ago.`);
+				const latestRecord = result[result.length - 1];
+				const updatedAt = moment(latestRecord.updatedAt);
+				console.info('updated at', updatedAt.format('YYYY-MM-DD HH:mm:ss'));
 
+				const timeDiff = moment().diff(updatedAt, 'days');
+				console.info(`last update was ${timeDiff} days ago.`);
+
+				const interval = 1; // 1 day
 				if (timeDiff > interval) {
 					console.info('data was too old. updating ...');
 					// get fresh data from strava and save into db
 					getElevationFromStrava()
 						.then(elevation => {
-							saveCurrentElevation(db, elevation);
+							saveCurrentElevation(db, elevation, (data) => {
+								res.render('index', {
+									currentMeters: data.name,
+									targetMeters: 100000,
+									title: '#BEATTODAY'
+								});
+							});
 						});
 						// render with `elevation`
 				} else {
-					console.info('still fresh data na');
+					console.info('using data from db');
 					// render with current value from db
-				}
-			}
-
-		 if (result.length === 0) {
-			 // get data from strava api and save into the db
-
-			const url = `${BASEURL}/athletes/${config.PROFILE_ID}/stats`;
-
-			// make api request to strava api
-			request.get({
-				url: url,
-				headers: {
-					Authorization: `Bearer ${config.API_KEY}`
-				}
-			}, (err, response, body) => {
-				if (!err && response.statusCode === 200) {
-					// grab json
-					// parse
-					const result = JSON.parse(body);
-					console.log('got data from strava api');
-					const now = new Date().getTime();
-					const elevation = {
-						name: result.all_ride_totals.elevation_gain,
-						updatedAt: now
-					};
-					db.collection('elevation').save(elevation, (err, result) => {
-						if (err) {
-							return console.error(err);
-						}
-
-						console.log('saved into db');
-						res.render('index', {
-							currentMeters: elevation.name,
-							targetMeters: 100000,
-							title: '#Y0LO'
-						})
+					res.render('index', {
+						currentMeters: latestRecord.name,
+						targetMeters: 100000,
+						title: '#BEATYESTERDAY'
 					});
-				} else {
-					console.error(err);
 				}
-			});
-		 } else {
-			 console.log('got value from db', result[0]);
-			 res.render('index', {
-				currentMeters: result[0].name,
-				targetMeters: 100000,
-				title: '#YOLO'
-			 });
-		 }
+			} else {
+				console.info('db is empty. getting new data from strava');
+				// @TODO - duplicates
+				getElevationFromStrava()
+					.then(elevation => {
+						saveCurrentElevation(db, elevation, (data) => {
+							res.render('index', {
+								currentMeters: data.name,
+								targetMeters: 100000,
+								title: '#FRESHMAN'
+							});
+						});
+					});
+			}
 	 });
 	});
 });
 
+/**
+ * get elevation data from strava api
+ */
 function getElevationFromStrava() {
 	const options = {
 		uri: `${BASEURL}/athletes/${config.PROFILE_ID}/stats`,
@@ -119,17 +101,24 @@ function getElevationFromStrava() {
 	return rp(options).then(response => response.all_ride_totals.elevation_gain);
 }
 
-function saveCurrentElevation(db, elevation) {
+/**
+ * save elevation data into the db, with `updatedAt` field
+ * which will be used to determine if the app should update
+ * data from api or not in the next page loads
+ */
+function saveCurrentElevation(db, elevation, cb) {
 	const data = {
 		name: elevation,
 		updatedAt: new Date().getTime()
 	};
+
 	db.collection('elevation').save(data, (err, result) => {
 		if (err) {
 			return console.error(err);
 		}
 
 		console.log('saved into db');
+		cb(data);
 	});
 }
 
