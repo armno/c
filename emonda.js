a72d5dffc09610ec2585222c35ea32258fc07331
Armno <monkeyarmno@gmail.com>
2016-11-04 14:59:48 +0700

    save emonda rides to the db

--- emonda.js
+++ emonda.js
@@ -2,13 +2,19 @@
 
 const config = require('./config');
 const rp = require('request-promise');
+const MongoClient = require('mongodb').MongoClient;
 
+/**
+ * get all rides of the Émonda
+ * @return {promise}
+ */
 function getEmondaActivities() {
 	const EMONDA_ID = 'b3027991';
 	const RIDE_TYPE = 'Ride';
 	const BASEURL = `https://www.strava.com/api/v3`;
+	const FIRST_RIDE_DATE = '1467849600'; // 7/7/2016
 	const options = {
-		uri: `${BASEURL}/athletes/${config.PROFILE_ID}/activities?after=1467849600&per_page=200`,
+		uri: `${BASEURL}/athletes/${config.PROFILE_ID}/activities?after=${FIRST_RIDE_DATE}&per_page=200`,
 		headers: {
 			Authorization: `Bearer ${config.API_KEY}`
 		},
@@ -23,9 +29,40 @@ function getEmondaActivities() {
 		});
 }
 
+/**
+ * save all rides into the db
+ * @param  {array} activities
+ * @return
+ */
 function saveEmondaActivites(activities) {
-	// save in mongodb
-	// what to save?
+	let simplifiedActivities = activities.map(activity => {
+		return {
+			activity_id: activity.id,
+			name: activity.name,
+			distance: activity.distance,
+			total_elevation_gain: activity.total_elevation_gain,
+			start_date: activity.start_date,
+			elev_high: activity.elev_high,
+			elev_low: activity.elev_low
+		};
+	});
+
+	MongoClient.connect('mongodb://localhost:27017/c', (err, db) => {
+		if (err) {
+			throw err;
+		}
+
+		db.collection('rides').insert(simplifiedActivities, (e) => {
+			if (e) {
+				console.error('error on saving data');
+				console.error(e);
+				return;
+			}
+
+			console.info(`saved ${simplifiedActivities.length} records`);
+			process.exit();
+		});
+	});
 }
 
 getEmondaActivities()
