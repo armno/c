35f9d4c77791efcd94118c014b27709a36e70bae
Armno <monkeyarmno@gmail.com>
2016-11-15 12:29:34 +0700

    display activities by newest-first. display other information of total rides in the header.

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,36 @@ const target = 100000;
 
 const Ride = require('../db/rides').Ride;
 
+/**
+ * summarize total of `prop`
+ * @param  {array} rides
+ * @param  {string} prop
+ * @return {number}
+ */
+function sumThings(rides, prop) {
+	return rides.reduce((a, b) => {
+		return a + b[prop];
+	}, 0);
+}
+
+/**
+ * sort activities array by newest-first
+ * @param  {Ride} a
+ * @param  {Ride} b
+ * @return {number}
+ */
+function sortActivities(a, b) {
+	if (a.activity_id > b.activity_id) {
+		return -1;
+	}
+
+	if (a.activity_id < b.activity_id) {
+		return 1;
+	}
+
+	return 0;
+}
+
 router.get('/', (req, res, next) => {
 
 	Ride.find({}, (err, rides) => {
@@ -16,9 +46,7 @@ router.get('/', (req, res, next) => {
 			throw err;
 		}
 
-		var meters = rides.reduce((a, b) => {
-			return a + b.total_elevation_gain;
-		}, 0);
+		var meters = sumThings(rides, 'total_elevation_gain');
 
 		// get data from `rides` collection
 		// sum all records to get current elevation
@@ -37,8 +65,20 @@ router.get('/logs', (req, res, next) => {
 			throw err;
 		}
 
+		rides.sort(sortActivities);
+
+		// number of Rides
+		// total elevation gain
+		// total distance
+		const numRides = rides.length;
+		const elevations = sumThings(rides, 'total_elevation_gain').toFixed(2);
+		const distance = (sumThings(rides, 'distance') / 1000).toFixed(2);
+
 		res.render('logs', {
-			rides: rides
+			rides,
+			numRides,
+			elevations,
+			distance
 		});
 	});
 });
