6550b2869a43ad531c0f9e1a63e8dfb11460aed0
Armno P <monkeyarmno@gmail.com>
2016-11-15 12:35:49 +0700

    Display logs in a new page `/logs` (#25). Fixes #14.
    * fix code style issues
    
    * create new route to display logs
    
    * display more data in logs page. move footer to partial view.
    
    * display activities by newest-first. display other information of total rides in the header.

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ var webhookHandler = GitHubWebhook({ path: '/pull', secret: config.GITHUB_SECRET
 var exec = require('child_process').exec;
 
 var routes = require('./routes/index');
+var moment = require('moment');
 
 var app = express();
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('formatDate', (datetime) => {
+	return moment(datetime).format('MM.DD.YYYY');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,9 +37,9 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -41,23 +47,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 webhookHandler.on('push', function (repo, data) {
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,36 @@ const target = 100000;
 
 const Ride = require('../db/rides').Ride;
 
+/**
+ * summarize total of `prop`
+ * @param  {array} rides
+ * @param  {string} prop
+ * @return {number}
+ */
+function sumThings(rides, prop) {
+	return rides.reduce((a, b) => {
+		return a + b[prop];
+	}, 0);
+}
+
+/**
+ * sort activities array by newest-first
+ * @param  {Ride} a
+ * @param  {Ride} b
+ * @return {number}
+ */
+function sortActivities(a, b) {
+	if (a.activity_id > b.activity_id) {
+		return -1;
+	}
+
+	if (a.activity_id < b.activity_id) {
+		return 1;
+	}
+
+	return 0;
+}
+
 router.get('/', (req, res, next) => {
 
 	Ride.find({}, (err, rides) => {
@@ -16,18 +46,41 @@ router.get('/', (req, res, next) => {
 			throw err;
 		}
 
-		var meters = rides.reduce((a, b) => {
-			return a + b.total_elevation_gain;
-		}, 0);
+		var meters = sumThings(rides, 'total_elevation_gain');
 
 		// get data from `rides` collection
 		// sum all records to get current elevation
 		res.render('index', {
 			currentMeters: meters.toFixed(0).toLocaleString(),
-			targetMeters: 100000
+			targetMeters: 100000,
+			containerClass: 'flex'
 		});
 	});
 
+});
+
+router.get('/logs', (req, res, next) => {
+	Ride.find({}, (err, rides) => {
+		if (err) {
+			throw err;
+		}
+
+		rides.sort(sortActivities);
+
+		// number of Rides
+		// total elevation gain
+		// total distance
+		const numRides = rides.length;
+		const elevations = sumThings(rides, 'total_elevation_gain').toFixed(2);
+		const distance = (sumThings(rides, 'distance') / 1000).toFixed(2);
+
+		res.render('logs', {
+			rides,
+			numRides,
+			elevations,
+			distance
+		});
+	});
 });
 
 module.exports = router;
