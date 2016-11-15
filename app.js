26b6c11e07d6fefa03b7685f1f81972e1476fee9
Armno <monkeyarmno@gmail.com>
2016-11-15 12:01:33 +0700

    display more data in logs page. move footer to partial view.

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ var webhookHandler = GitHubWebhook({ path: '/pull', secret: config.GITHUB_SECRET
 var exec = require('child_process').exec;
 
 var routes = require('./routes/index');
+var moment = require('moment');
 
 var app = express();
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('formatDate', (datetime) => {
+	return moment(datetime).format('MM.DD.YYYY');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
